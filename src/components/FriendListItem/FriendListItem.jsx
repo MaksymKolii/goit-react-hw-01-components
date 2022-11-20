757c9cc3f457ee3dd80666aa403724047c5bc993
Maksym Kolii <maksymkolii@gmail.com>
2022-11-20 22:53:26 +0100

    commit

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,13 +1,12 @@
 import PropTypes from 'prop-types';
-import { Fragment } from 'react';
 import { Status, FriendName, Avatar } from './FriendListItem.styled';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => (
-  <Fragment>
+  <>
     <Status on={isOnline.toString()}></Status>
     <Avatar src={avatar} alt={name} width="48" />
     <FriendName>{name}</FriendName>
-  </Fragment>
+  </>
 );
 
 FriendListItem.propTypes = {
